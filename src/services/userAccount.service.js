789987f50c1036753dc9efcea54b7b427b519bc8
lashu <lashurov44@gmail.com>
2025-12-20 23:22:49 +0200

    Add UserAccountService with skeleton methods for user management

---
+++ src/services/userAccount.service.js
@@ -0,0 +1,21 @@
+class UserAccountService {
+    async register(user){
+        // TODO: add new user and return user profile
+    }
+
+    async getUser(login){
+        //TODO: return user profile
+    }
+
+    async removeUser(login){
+        //TODO: remove user and return user profile
+    }
+
+    async updateUser(login, user){
+        //TODO: update user profile and return user profile
+    }
+
+    async changeRoles(login, role, isAddRole){
+        //TODO: add or remove role and return user profile
+    }
+}

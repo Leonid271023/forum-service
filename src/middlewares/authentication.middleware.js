5bb49c35cf2312dc322e650b5099ada47853917e
lashu <lashurov44@gmail.com>
2025-12-27 16:40:35 +0200

    Refactor and unify authorization middlewares by replacing role-based and ownership-specific middleware with a single reusable `Authorization` class. Update route definitions accordingly.

--- src/middlewares/authentication.middleware.js
+++ src/middlewares/authentication.middleware.js
@@ -1,34 +1,21 @@
 import UserAccount from '../models/user.model.js';
 
 const authentication = async (req, res, next) => {
+    if (req.path !== '/account/register' && !req.path.startsWith('/forum/posts')) {
+        const authorization = req.headers.authorization;
+        if (!authorization || !authorization.startsWith('Basic')) return res.status(401).json({message: 'Authorization required'});
+        const token = authorization.split(' ')[1];
+        const decodedToken = Buffer.from(token, 'base64').toString('ascii');
+        const [login, password] = decodedToken.split(':');
+        const userAccount = await UserAccount.findById(login);
 
-    if (req.method === 'GET' && req.path === '/forum/posts') {
-        return next();
+        if (!userAccount || !(await userAccount.comparePassword(password))) {
+            return res.status(401).json({message: 'Invalid credentials'});
+        }
+        req.headers.authorization = '';
+        req.principal = {username: login, roles: userAccount.roles};
     }
-
-    if (req.method === 'POST' && req.path === '/account/register') {
-        return next();
-    }
-
-
-    const authorization = req.headers.authorization;
-    if (!authorization || !authorization.startsWith('Basic')) {
-        return res.status(401).json({ message: 'Authorization required' });
-    }
-
-    const token = authorization.split(' ')[1];
-    const decodedToken = Buffer.from(token, 'base64').toString('ascii');
-    const [login, password] = decodedToken.split(':');
-
-    const userAccount = await UserAccount.findById(login);
-    if (!userAccount || !(await userAccount.comparePassword(password))) {
-        return res.status(401).json({ message: 'Invalid credentials' });
-    }
-
-    req.headers.authorization = '';
-    req.principal = { username: login, roles: userAccount.roles };
-
-    return next();
-};
+    return next()
+}
 
 export default authentication;
--- src/routes/userAccount.router.js
+++ src/routes/userAccount.router.js
@@ -1,18 +1,16 @@
 import {Router} from "express";
 import userAccountController from "../controillers/userAccount.controller.js";
 import validate from "../middlewares/validation.middleware.js";
-import ownerOrAdmin from "../middlewares/ownerOrAdmin.middleware.js";
-import onlyAdmin from "../middlewares/onlyAdmin.middleware.js";
 
 const router = Router();
 
 
 router.post('/register', validate('register'), userAccountController.register);
 router.post('/login', userAccountController.login);
-router.delete('/user/:user',ownerOrAdmin('user'), userAccountController.deleteUser);
+router.delete('/user/:user', userAccountController.deleteUser);
 router.patch('/user/:user', validate('updateUser'), userAccountController.updateUser);
-router.patch('/user/:user/role/:role',onlyAdmin, validate('changeRoles', 'params'), userAccountController.addRole);
-router.delete('/user/:user/role/:role',onlyAdmin, validate('changeRoles', 'params'), userAccountController.deleteRole);
+router.patch('/user/:user/role/:role', validate('changeRoles', 'params'), userAccountController.addRole);
+router.delete('/user/:user/role/:role', validate('changeRoles', 'params'), userAccountController.deleteRole);
 router.patch('/password', userAccountController.changePassword);
 router.get('/user/:user', userAccountController.getUser);
 
